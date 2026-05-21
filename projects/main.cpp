c4c21779ef27c2c79f4ea24436a6fc7edb54fac7
Kaisar <qaisarshalqar@gmail.com>
2026-05-22 00:31:38 +0500

    add main.cpp

---
+++ projects/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+    int san, san2;
+    std::cout << "Write the first number: "; std::cin >> san;
+    std::cout << "Write the second number: "; std::cin >> san2;
+    std::cout << "The sum of your two number is: " << san + san2 << "\n";
+    return 0;
+}
