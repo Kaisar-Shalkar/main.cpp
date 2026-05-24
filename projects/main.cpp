6ffac0d58362f525794fef8cc660155bfd0ee4f8
Kaisar <qaisarshalqar@gmail.com>
2026-05-24 22:55:40 +0500

    Add error

--- projects/main.cpp
+++ projects/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 using namespace std;
 int main(){
     int san1, san2;
@@ -7,17 +6,20 @@ int main(){
     cout << "Write the first number: "; cin >> san1;
     cout << "Write the second number: "; cin >> san2;
     cout << "What do you want to do: "; cin >> sign;
-    if(sign == '*') {
+    if (sign == '*') {
         cout << "Answer is: " << san1 * san2 << endl;
     }
-    if(sign == '/') {
+    else if (sign == '/') {
         cout << "Anser is: " << san1 / san2 << endl;
     }
-    if(sign == '+') {
+    else if (sign == '+') {
         cout << "Anser is: " << san1 + san2 << endl;
     }
-    if(sign == '-') {
+    else if (sign == '-') {
         cout << "Anser is: " << san1 - san2 << endl;
+    }
+    else {
+        cout << "Error" << "\n";
     }
     return 0;
 }
